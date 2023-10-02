34b2eb5afee3065d6a1f2c0943ba2d1aa416641c
mujibat <oshikoyadolapo13@gmail.com>
2023-10-02 21:28:01 +0100

    lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,61 +3,91 @@ pragma solidity 0.8.19;
 
 contract Lottery {
    
-    bool lotteryOpen;
-    uint deadline;
-    uint public WinAmount;
-    address[] ticketbuyers;
+    
+    address private winningticket;
+    uint private profitAmount;
     address manager;
 
+    struct LotteryUser {
+        address[] ticketbuyers;
+        address owner;
+        uint deadline;
+        bool lotteryActive;
+        string name;
+    }
+    uint _ID;
 
     constructor() {
          manager = msg.sender;
     }
+    // id to balance
+    mapping(uint => uint) lotteryBalance;
+    mapping(uint => LotteryUser) public loter;
+    mapping(uint => mapping(address => uint)) public ticketbuyer;
+     mapping(uint => mapping(address => bool)) public hasBoughtTicket;
+
     modifier onlyManager() {
         require(msg.sender == manager, "Only Manager");
         _;
     }
 
-
-    function createLottery( uint _durationTime) external payable onlyManager{
-       deadline = _durationTime +  block.timestamp;
-       lotteryOpen = true;
+    function createLottery(uint _durationTime, string memory _name) external payable {
+        _ID++;
+        LotteryUser storage lottery =  loter[_ID];
+       lottery.deadline = _durationTime +  block.timestamp;
+       lottery.lotteryActive = true;
+       lottery.owner = msg.sender;
+       lottery.name = _name;
     }
 
-    function buyLotteryTicket() external payable {
-         require(deadline >= block.timestamp, "Lottery Ended");
-        require(msg.value >= 2, "Input higher amount");
-        ticketbuyers.push(msg.sender);
+    function buyLotteryTicket(uint ID) external payable {
+        LotteryUser storage lottery =  loter[ID];
+        if(msg.sender == address(0)) {
+            payable(manager).transfer(msg.value);
+        }
+        ticketbuyer[ID][msg.sender] += msg.value;
+        lotteryBalance[ID] += msg.value;
+        // winAmount += msg.value;
+         require(lottery.deadline >= block.timestamp, "Lottery Ended");
+        require(msg.value > 0, "Input higher amount");
+        if (!hasBoughtTicket[ID][msg.sender]) {
+           lottery.ticketbuyers.push(msg.sender); 
+        hasBoughtTicket[ID][msg.sender] = true;
+        }
     }
     
-      function random() internal view returns(uint randomNumber) {
-        randomNumber = (uint(keccak256(abi.encodePacked( block.timestamp, ticketbuyers))) % ticketbuyers.length) + 1;
+      function random(uint ID) internal view returns(uint randomNumber) {
+        LotteryUser storage lottery =  loter[ID];
+        randomNumber = (uint(keccak256(abi.encodePacked( block.timestamp, lottery.ticketbuyers))) % lottery.ticketbuyers.length) + 1;
     }
 
-    function winningAddress() external view returns (address winningticket) {
-        require(block.timestamp > deadline, "Lottery ended");
-        uint winningnumber = random();
-        winningticket = ticketbuyers[winningnumber];
+    function getWinner(uint ID) external payable onlyManager returns(address) {
+        LotteryUser storage lottery =  loter[ID];
+        require(lottery.ticketbuyers.length > 2, "Need more participants");
+        require(block.timestamp > lottery.deadline, "Lottery ended");
+        lottery.lotteryActive = false;
+        uint winningnumber = random(ID);
+        winningticket = lottery.ticketbuyers[winningnumber];
+        uint winAmount = lotteryBalance[ID] * 95 /100;
+        profitAmount = lotteryBalance[ID] * 5 / 100;
+        payable(winningticket).transfer( winAmount);
+        payable(manager).transfer(profitAmount);
+        return winningticket;
+    }
+
+    function getProfit() internal view returns (uint profit) {
+        profit = (address(this).balance * 5) / 100;
     }
     function balanceOfLotteryHouse() public view returns (uint balance) {
          balance = address(this).balance;
     }
     
-    function transferWinAmount() external onlyManager{
-        require(block.timestamp > deadline, "Lottery In Session");
-        WinAmount = address(this).balance * 95 /100;
-        address winner = ticketbuyers[random()];
-        payable(winner).transfer( WinAmount);
-        ticketbuyers = new address[](0);
-        lotteryOpen = false;
-    }
-    function withdrawProfit() external onlyManager{
-        uint profit = address(this).balance * 5 / 100;
-        payable(manager).transfer(profit);
-    }
-    function getPlayers() external view returns(uint){
-        return ticketbuyers.length;
+ 
+    function getTicketBuyers(uint ID) external view returns(address [] memory){
+        LotteryUser storage lottery =  loter[ID];
+        return lottery.ticketbuyers;
     }
 
     
+   
 }
